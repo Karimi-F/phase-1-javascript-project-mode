e069465ff5d7f5dda0c04d89b1f2103b81d64235
Karimi-F <fridah37karimi@gmail.com>
2024-10-16 10:57:57 +0300

    Modifies the project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Event Listeners to be used
+// 1. OnSubmit
+// 2. Onclick
+// 3. On Toggle
+// 4. Double Click
+
+// Array iterator method to be used
+// 1. Filter
+// 2. Map
+// 3. For Each
+
+// Fetch methods to be used
+// 1. GET
+// 2. POST
+// 3. PATCH
+// 4. DELETE
+
